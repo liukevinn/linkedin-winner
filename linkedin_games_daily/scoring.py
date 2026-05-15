1f52c0efe66336905873dbde109572a987c4bb67
Kevin Liu <kevinwliuu@gmail.com>
2026-05-14 22:56:36 -0500

    deleted all commentss

--- linkedin_games_daily/scoring.py
+++ linkedin_games_daily/scoring.py
@@ -130,11 +130,3 @@ def compute_standings(
     return per_game, totals
 
 
-##this commit is a change to rest the git capabilities
-
-###added this coment 10:03
-
-
-
-#Testing amending commmits - commit 1 (before ammending it)
-#adjustmnet text to test the ammend (after)
